846a1bec3814465ff6bc3c10ae35b08eac53b928
jphdevsf <62826600+jphdevsf@users.noreply.github.com>
2020-03-29 23:30:21 -0700

    started rhyme time app

---
+++ app.js
@@ -0,0 +1,57 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+const api_helper = require('./API_helper')
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+/*
+* Route to DEMO the API call to a REST API Endpoint 
+* REST URL : https://jsonplaceholder.typicode.com/todos/1
+*/
+app.get('/', (req, res) => {
+  api_helper.make_API_call('https://rhymebrain.com/talk?function=getRhymes&word=house')
+  .then(response => {
+      res.json(response)
+  })
+  .catch(error => {
+      res.send(error)
+  })
+})
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
