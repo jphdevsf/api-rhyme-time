26ee10d5072c13d96a9abc5b7074d9c7bc87e922
jphdevsf <62826600+jphdevsf@users.noreply.github.com>
2020-04-07 23:42:24 -0700

    implemented server-side validation though could use some refactoring

--- routes/_router.js
+++ routes/_router.js
@@ -1,5 +1,7 @@
 const bodyParser = require('body-parser')
 const fetch = require('node-fetch')
+const { check, validationResult } = require('express-validator')
+// const { sanitizeBody } = require('express-validator/filter')
 
 const myRoutes = (app) => {
   // for parsing application/json
@@ -14,11 +16,23 @@ const myRoutes = (app) => {
       errors: {}
     })
   })
-  app.post('/', (req, res) => {
+
+  app.post('/', [
+    check('rhymeText')
+      .matches(/^[a-zA-Z]+$/).withMessage('Only letters are allowed.')
+      .isLength({ min: 1, max: 15 }).withMessage('Please enter between 1 and 15 letters.')
+  ], (req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      res.render('index', { title: 'Error | Rhyme Time', errorsArray: errors.array() })
+    }
+    // if above passes, go get the rhymes!
     const formData = req.body
+    console.log(formData)
     //! maxResults selection from form is not working. Use Multer for multi line form data?
-    const maxResults = formData.maxResults ? formData.maxResults : 100
-    const reqUrl = `https://rhymebrain.com/talk?function=getRhymes&word=${formData.text}&maxResults=${maxResults}`
+    const maxResults = formData.maxResults ? formData.maxResults : 10
+    const reqUrl = `https://rhymebrain.com/talk?function=getRhymes&word=${formData.rhymeText}&maxResults=${maxResults}`
+
     fetch(reqUrl)
       .then((response) => {
         if (!response.ok) {
@@ -28,16 +42,18 @@ const myRoutes = (app) => {
       })
       .then((myData) => {
         // res.send(data)
-        res.render('index', { title: 'Results | Rhyme Time', wordSearched: formData.text, resultsArray: myData })
+        res.render('index', { title: 'Results | Rhyme Time', wordSearched: formData.rhymeText, resultsArray: myData })
       })
       .catch((error) => {
         console.error('Oops, Fetch Operation Error:', error)
       })
   })
+
   app.use((req, res) => {
     res.status(400)
     res.render('404.pug', { title: '404: File Not Found' })
   })
+
   app.use((error, req, res, next) => {
     res.status(500)
     res.render('500.pug', { title: '500: Internal Server Error', error: error })
